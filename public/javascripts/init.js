367a0cc849cd3b3643f820ecd4c7452beef7ced0
Adam Chamberlin <adam@shibbyonline.co.uk>
2013-12-19 14:49:42 +0000

    add url fragments

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -19,8 +19,9 @@ function initialize() {
   var map = new google.maps.Map(document.getElementById('map'), myOptions);
   
   google.maps.event.addListener(map, 'dragend', function(){
-    console.log("User dragged the map");
     map.clearOverlays();
+    $(".sidebar h1").text('Map moved, reloading.');
+    $('#feedback').html('');
     $('#feedback').html('<p class="feedback">Looking up the local Stava segments</p>');
     var ne = map.getBounds().getNorthEast();
     var sw = map.getBounds().getSouthWest();
@@ -33,14 +34,38 @@ function initialize() {
           map.setCenter(marker.getPosition());
           display_segment(item);
         });
-    	});
+      });
     });
+    $(".sidebar h1").text('Select a KOM');
+    $(".distance").text('');
+    $(".climb").html('');
+    $(".link__ss").html('');
     $('#feedback').html('');
+    window.location = '#' + sw.lat() + ',' + sw.lng() + ',' + ne.lat() + ',' + ne.lng();
   });
   return map;
 }
 
 map = initialize();
+
+if(window.location.hash) {
+  var hash = window.location.hash.substring(1).split(',');
+  map.fitBounds(new google.maps.LatLngBounds(new google.maps.LatLng(hash[0],hash[1]),new google.maps.LatLng(hash[2],hash[3])));
+  $.getJSON("/segments/" + hash[0] + "/" + hash[1] + "/" + hash[2] + "/" + hash[3], function(data){
+    $.each(data.segments, function(i,item){
+      var marker = dropPin(item.start_latlng[0], item.start_latlng[1]);
+      markers.push(marker);
+      google.maps.event.addListener(marker, 'click', function() {
+        map.setCenter(marker.getPosition());
+        display_segment(item);
+      });
+  	});
+  });
+  
+  $('#feedback').html('');
+  $(".content").fadeOut(800);
+  $(".sidebar").addClass('open', 1200);
+}
 
 function initiate_geolocation() {
   if (navigator.geolocation) {
@@ -79,17 +104,24 @@ function handle_location_query(position) {
 function handle_error(error) {
   switch(error.code)
   {
-    case error.PERMISSION_DENIED: $('#feedback').html('<p class="error">You did not share your geolocation data.<br />You need to allow your browser to use your location, or use the map below and click on your location</p></p>');
+    case error.PERMISSION_DENIED:
+      $(".sidebar h1").text('Whoops, problems.');
+      $('#feedback').html('<p class="error">You did not share your geolocation data.<br />You need to allow your browser to use your location.</p>');
     break;
 
-    case error.POSITION_UNAVAILABLE: $('#feedback').html('<p class="error">Could not detect current position.<br />Please use the map below and click on your location</p>');
-    handle_location_query();        
+    case error.POSITION_UNAVAILABLE:
+      $(".sidebar h1").text('Whoops, problems.');
+      $('#feedback').html('<p class="error">Could not detect current position.<br />This some times happens if your on commerical networks, best to use a mobile device.</p>');       
     break;
 
-    case error.TIMEOUT: $('#feedback').html('<p class="error">Retrieving position timed out.<br />Please use the map below and click on your location</p></p>');
+    case error.TIMEOUT:
+      $(".sidebar h1").text('Whoops, problems.');
+      $('#feedback').html('<p class="error">Retrieving position timed out.</p>');
     break;
 
-    default: $('#feedback').html('<p class="error">Unknown error.<br />Please use the map below and click on your location</p></p>');
+    default:
+      $(".sidebar h1").text('Whoops, problems.');
+      $('#feedback').html('<p class="error">Unknown error.</p>');
     break;
   }
 }
